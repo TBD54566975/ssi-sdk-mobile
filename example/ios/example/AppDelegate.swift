c8e90c9792b1a0523df1406fcf9da46b56fc47ee
Tim Shamilov <shamilovtim@gmail.com>
2023-02-07 09:46:00 -0700

    init iOS example app

---
+++ example/ios/example/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  example
+//
+//  Created by Tim Shamilov on 2/7/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ example/ios/example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  example
+//
+//  Created by Tim Shamilov on 2/7/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
