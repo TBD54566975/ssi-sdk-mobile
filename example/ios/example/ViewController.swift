6ffd3893e92a9a3faf18544d4a71f37ec4ec9ce6
Tim Shamilov <shamilovtim@gmail.com>
2023-02-07 21:39:08 -0700

    ios example: swap out example

--- example/ios/example/ViewController.swift
+++ example/ios/example/ViewController.swift
@@ -5,20 +5,14 @@
 //  Created by Tim Shamilov on 2/7/23.
 //
 
-import Cocoa
+import UIKit
+import Mobile
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
