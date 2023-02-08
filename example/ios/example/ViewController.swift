e96e012dbf10ba8983f01ca8fb63b4adcd606816
Tim Shamilov <shamilovtim@gmail.com>
2023-02-08 01:11:40 -0700

    ios example: add create did button. crashing.

--- example/ios/example/ViewController.swift
+++ example/ios/example/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var error: NSError? = NSError()
+        
+//        let supportedKeyTypes = IdentityGetSupportedKeyTypes();
+        let did = IdentityGenerateDIDKey("RSA", &error);
+        
+        print(did?.didKey);
+        print(did?.privateKey);
     }
 
 
