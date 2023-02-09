81c25eaca73fc4758c481f6a10229ca8df215487
Tim Shamilov <shamilovtim@gmail.com>
2023-02-08 19:32:15 -0700

    ios example: create did with button. add logger.

--- example/ios/example/ViewController.swift
+++ example/ios/example/ViewController.swift
@@ -9,26 +9,40 @@ import UIKit
 import Identity
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var LittleConsole: UILabel!
+    @IBAction func CreateDidTapped(_ sender: Any) {
+        createDID();
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        var error: NSError? = NSError()
+        addLog(text: "App initialized.");
+    }
+    
+    func createDID() {
+        // dont think we need this
+        // let supportedKeyTypes = IdentityGetSupportedKeyTypes();
         
-//        let supportedKeyTypes = IdentityGetSupportedKeyTypes();
+        var error: NSError? = NSError()
         let did = IdentityGenerateDIDKey("secp256k1", &error);
         
-        print(did?.didKey);
         if let unwrapped = did?.privateJSONWebKey {
             do {
                 let json = try JSONSerialization.jsonObject(with: unwrapped);
-                print(json);
+                addLog(text: "Did creation successful. Did is: \(did!.didKey)");
+                addLog(text: "Did JSON is: \(json)");
             } catch {
-                print("Error while parsing JSON: \(error)");
+                addLog(text: "Error while parsing JSON: \(error)")
             }
         }
     }
-
-
+    
+    // utils
+    func addLog(text: String) {
+        let currentLogs = self.LittleConsole.text ?? "";
+        self.LittleConsole.text = currentLogs + text + "\n\n";
+        print(text);
+    }
 }
 
