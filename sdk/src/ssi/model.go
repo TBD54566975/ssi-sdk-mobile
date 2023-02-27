f4220089640d4dd5178bb2bf56b7da599717bc8e
Adam Mika <amika@squareup.com>
2023-02-27 15:19:41 -0700

    Trying to get VerifiableCredentials struct to work

--- sdk/src/ssi/model.go
+++ sdk/src/ssi/model.go
@@ -1,5 +1,9 @@
 package ssi
 
+import (
+	"github.com/TBD54566975/ssi-sdk/credential"
+)
+
 type DIDDocumentMobile struct {
 	Context              *StringArray                `json:"@context,omitempty"`
 	ID                   string                      `json:"id,omitempty"`
@@ -13,3 +17,49 @@ type DIDDocumentMobile struct {
 	CapabilityDelegation *VerificationMethodSetArray `json:"capabilityDelegation,omitempty" validate:"dive"`
 	Services             *ServiceSetArray            `json:"service,omitempty" validate:"dive"`
 }
+
+type VerifiableCredentialMobile struct {
+	Context *StringArray `json:"@context" validate:"required"`
+	ID      string       `json:"id,omitempty"`
+	Type    *StringArray `json:"type" validate:"required"`
+
+	// Mika: Problem type
+	// Either a string or an object
+	// Issuer         interface{} `json:"issuer" validate:"required"`
+
+	IssuanceDate   string `json:"issuanceDate" validate:"required"`
+	ExpirationDate string `json:"expirationDate,omitempty"`
+
+	// Mika: Problem type
+	// Requires id and type, but anything else is fair game
+	// CredentialStatus  interface{}       `json:"credentialStatus,omitempty" validate:"omitempty,dive"`
+
+	// Mika: Problem type
+	// type is: map[string]interface{}
+	// CredentialSubject CredentialSubject            `json:"credentialSubject" validate:"required"`
+
+	// Mika: Problem type
+	// These don't work, even though the structs only contain supported types
+	CredentialSchema *credential.CredentialSchema `json:"credentialSchema,omitempty" validate:"omitempty,dive"`
+	RefreshService   *credential.RefreshService   `json:"refreshService,omitempty" validate:"omitempty,dive"`
+
+	// Mika: Problem type
+	// TermsOfUse       []TermsOfUse                 `json:"termsOfUse,omitempty" validate:"omitempty,dive"`
+
+	// Mika: Problem type
+	// Evidence         []interface{}                `json:"evidence,omitempty" validate:"omitempty,dive"`
+
+	// Mika: Problem type
+	// Proof            *crypto.Proof                `json:"proof,omitempty"`
+}
+
+// Mika: Can I avoid needing to re-declare these structs?
+// type CredentialSchema struct {
+// 	ID   string `json:"id" validate:"required"`
+// 	Type string `json:"type" validate:"required"`
+// }
+//
+// type RefreshService struct {
+// 	ID   string `json:"id" validate:"required"`
+// 	Type string `json:"type" validate:"required"`
+// }
