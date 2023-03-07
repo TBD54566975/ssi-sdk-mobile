007eb720592d1d6d886a7eeb520a5ce437414587
Adam Mika <amika@squareup.com>
2023-03-07 15:19:20 -0700

    fix expand button not being disabled until did created

--- react-native-ssi/example/src/App.tsx
+++ react-native-ssi/example/src/App.tsx
@@ -37,7 +37,7 @@ export function App() {
               <Text style={styles.buttonText}>Generate DID</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              disabled={didKey == null}
+              disabled={didKey == ''}
               style={styles.button}
               onPress={() => {
                 expandDidKey(didKey).then((result) => {
