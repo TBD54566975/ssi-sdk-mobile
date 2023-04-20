c3148655efaaff49e8c8414533dbe7353c6b01c5
Adam Mika <88001738+amika-sq@users.noreply.github.com>
2023-04-20 15:24:34 -0600

    Credential Manifest types (#33)

--- react-native-ssi/src/types.ts
+++ react-native-ssi/src/types.ts
@@ -96,3 +96,187 @@ export type TermsOfUse = {
 export type Proof = {
   type: string | string[];
 };
+
+export type CredentialManifest = {
+  id: string;
+  spec_version: string;
+  issuer: CredentialIssuer;
+  output_descriptors: OutputDescriptor[];
+  name?: string;
+  description?: string;
+  format?: ClaimFormatDesignation;
+  presentation_definition?: PresentationDefinition;
+};
+
+export type CredentialIssuer = {
+  id: string;
+  name?: string;
+  styles?: EntityStyle;
+  comment?: string;
+};
+
+export type EntityStyleImage = {
+  uri: string;
+  alt?: string;
+};
+
+export type EntityStyleColor = {
+  color: string;
+};
+
+export type EntityStyle = {
+  thumbnail?: EntityStyleImage;
+  hero?: EntityStyleImage;
+  background?: EntityStyleColor;
+  text?: EntityStyleColor;
+};
+
+export type OutputDescriptor = {
+  id: string;
+  schema: string;
+  name?: string;
+  description?: string;
+  styles?: EntityStyle;
+  display?: DataDisplay;
+};
+
+export type DataMappingSchemaNonString = {
+  type: 'boolean' | 'number' | 'integer';
+};
+
+export type DataMappingSchemaString = {
+  type: 'string';
+  format?:
+    | 'date-time'
+    | 'time'
+    | 'date'
+    | 'email'
+    | 'idn-email'
+    | 'hostname'
+    | 'idn-hostname'
+    | 'ipv4'
+    | 'ipv6'
+    | 'uri'
+    | 'uri-reference'
+    | 'iri'
+    | 'iri-reference';
+};
+
+export type DataMappingSchema =
+  | DataMappingSchemaString
+  | DataMappingSchemaNonString;
+
+export type DataMappingText = {
+  text: string;
+};
+
+export type DataMappingPath = {
+  path: string[];
+  fallback?: string;
+  schema: DataMappingSchema;
+};
+
+export type DisplayMapping = DataMappingPath | DataMappingText;
+
+export type LabeledDisplayMapping = DisplayMapping & {
+  label: string;
+};
+
+export type DataDisplay = {
+  title?: DisplayMapping;
+  subtitle?: DisplayMapping;
+  description?: DisplayMapping | { text: string };
+  properties?: LabeledDisplayMapping[];
+};
+
+export type ClaimFormatDesignationAlg = {
+  alg: string[];
+};
+
+export type ClaimFormatDesignationProof = {
+  proof_type: string[];
+};
+
+export type ClaimFormatDesignation = {
+  jwt?: ClaimFormatDesignationAlg;
+  jwt_vc?: ClaimFormatDesignationAlg;
+  jwt_vp?: ClaimFormatDesignationAlg;
+  ldp_vc?: ClaimFormatDesignationProof;
+  ldp_vp?: ClaimFormatDesignationProof;
+  ldp?: ClaimFormatDesignationProof;
+};
+
+export type PresentationDefinition = {
+  id: string;
+  input_descriptors: InputDescriptor[];
+  name?: string;
+  purpose?: string;
+  format?: ClaimFormatDesignation;
+};
+
+export enum InputDescriptorConstraintStatusDirective {
+  REQUIRED = 'required',
+  ALLOWED = 'allowed',
+  DISALLOWED = 'disallowed',
+}
+
+export enum InputDescriptorConstraintDirective {
+  REQUIRED = 'required',
+  PREFERRED = 'preferred',
+}
+
+export type InputDescriptorConstraintStatus = {
+  directive: InputDescriptorConstraintStatusDirective;
+};
+
+export type InputDescriptorConstraintStatuses = {
+  active?: InputDescriptorConstraintStatus;
+  suspended?: InputDescriptorConstraintStatus;
+  revoked?: InputDescriptorConstraintStatus;
+};
+
+export type InputDescriptorConstraintSubjectConstraint = {
+  field_id: string[];
+  directive: InputDescriptorConstraintDirective;
+};
+
+export type InputDescriptorConstraintFilter = {
+  type: string;
+  format?: string;
+  pattern?: string;
+  minimum?: string | number;
+  minLength?: number;
+  maxLength?: number;
+  exclusiveMinimum?: string | number;
+  exclusiveMaximum?: string | number;
+  maximum?: string | number;
+  const?: string | number;
+  enum?: string[] | number[];
+  not?: InputDescriptorConstraintFilter;
+};
+
+export type InputDescriptorConstraintField = {
+  path: string[];
+  id?: string;
+  purpose?: string;
+  filter?: InputDescriptorConstraintFilter;
+  predicate?: InputDescriptorConstraintDirective;
+};
+
+export type InputDescriptorConstraints = {
+  limit_disclosure?: InputDescriptorConstraintDirective;
+  statuses?: InputDescriptorConstraintStatuses;
+  subject_is_issuer?: InputDescriptorConstraintDirective;
+  is_holder?: InputDescriptorConstraintSubjectConstraint[];
+  same_subject?: InputDescriptorConstraintSubjectConstraint[];
+  fields?: InputDescriptorConstraintField[];
+};
+
+export type InputDescriptor = {
+  id: string;
+  group?: string;
+  name?: string;
+  purpose?: string;
+  constraints?: InputDescriptorConstraints;
+  format?: ClaimFormatDesignation;
+};
